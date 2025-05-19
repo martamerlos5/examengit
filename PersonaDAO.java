791062913c9d1830d5e6f03920a8a207045121e1
marta <1456414@alu.murciaeduca.es>
2025-05-19 21:46:32 +0200

    1er commit examengit

---
+++ PersonaDAO.java
@@ -0,0 +1,92 @@
+package modelo;
+
+import java.sql.*;
+
+public class PersonaDAO {
+    //Cada modelo de datos tiene su modelo DAO con métodos para interactuar con las clases
+
+    public boolean insertar(Persona persona){
+        boolean devolver = false;
+        //pasar 3 datos con preparedstatement y hacer el insert
+        Connection con = BBDDConexion.conectar(); //se invoca a la funcion
+        if (persona != null) {
+            try {
+                //siempre se invoca un método Prepared
+                PreparedStatement pst = con.prepareStatement("insert into personas values (?,?,?)");
+                pst.setString(1, persona.getNombre());
+                pst.setInt(2, persona.getEdad());
+                pst.setInt(3, persona.getIdPoblacion().getId()); //CAMBIO
+
+                int resultado = pst.executeUpdate();
+                if (resultado > 0) {
+                    devolver = true;
+                }
+            } catch (SQLException ex) {
+                System.out.println("Error al conectarse a la BBDDS");
+            } finally {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+            }
+        }
+        return devolver;
+    }
+
+    public Persona buscar(String nombre) { //recibe el nombre porque es la clave primaria de la tabla
+        Connection con = BBDDConexion.conectar();
+        Statement st;
+        Persona persona = null;
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery("select t.nombre, t.edad, p.nombre 'nombrepoblacion', +"
+                    + "p.id from personas t join poblaciones p on "
+                    + "t.idPoblacion = p.id where lower(t.nombre) = lower('"+nombre+"');");
+            if (rs.next()) {
+                persona = new Persona();
+                Poblacion poblacion = new Poblacion();
+                poblacion.setId(rs.getInt("id")); //porque lo devuelve como id
+                poblacion.setNombre(rs.getString("nombrepoblacion")); //nombre de la poblacion del campo de la consulta . 2 porque poblacion tiene 2 atributos (campos)
+                
+                persona.setEdad(rs.getInt("edad"));
+                persona.setNombre(rs.getString("nombre"));
+                persona.setIdPoblacion(poblacion); //aqui se le pasa el objeto
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al conectarse a la BBDD");
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println("Error");
+            }
+        }
+        return persona;
+    }
+
+    //Se elimina por clave primaria (String nombre)
+    public int eliminar(String nombre) {
+        Connection conexion = BBDDConexion.conectar();
+        String sql = "DELETE FROM personas WHERE LOWER(nombre) = LOWER(?)";
+        PreparedStatement pst = null;
+        int res = 0;
+        try {
+            pst = conexion.prepareStatement(sql);
+            pst.setString(1, nombre);
+            res = pst.executeUpdate(); //esta ejecución es lo que se acaba devolviendo
+            System.out.println("Resultado al eliminar");
+        } catch (SQLException ex) {
+            System.out.println("Error al conectarse");
+        } finally {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.out.println("Error al conectarse a la BBDD");
+            }
+        }
+        return res;
+
+    }
+}
---
+++ PoblacionDAO.java
@@ -0,0 +1,40 @@
+package modelo;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PoblacionDAO {
+
+    //1 único método para recoger las poblaciones. Esto es porque es lo único que nos interesa de esa tabla
+    public ArrayList<Poblacion> getPoblaciones() {
+        ArrayList<Poblacion> lista = null;
+        Connection con = BBDDConexion.conectar();
+
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = con.createStatement();
+            //se le pasa la sentencia al ResultSet con executeQuery
+            rs = st.executeQuery("select * from poblaciones order by id"); //se ordenan por id las poblaciones para que esten en orden
+            lista = new ArrayList<>();//instanciar la lista
+            while (rs.next()) {
+                Poblacion p = new Poblacion();
+                p.setId(rs.getInt("id")); //los nombres de las variables, que son los campos de la tabla
+                p.setNombre(rs.getString("nombre"));
+                lista.add(p);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al conectarse a la BBDD");
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println("Error");
+            }
+
+        }
+
+        return lista;
+    }
+}
